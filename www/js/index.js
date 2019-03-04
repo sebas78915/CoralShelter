3b13683f46204256f14017039408de15f4caadd4
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-03-04 15:52:58 -0500

    falta los eventos
    muestra las imagenes random, las heridas, pero no asigna los eventos

--- www/js/index.js
+++ www/js/index.js
@@ -1,117 +1,131 @@
 //var cronometro;
-var intro, creditos, tortuga, foca, ballena, histTortuga, histFoca, histBallena;
 
-function desaparecer(){
-    var pantalla = document.getElementById("pantalla_carga");
-    pantalla.style.display = "none";
+window.onload = inicio;
+var heridas = [];
+var botones = [];
+var btnHeridas = [];
+var randomAnima = [];
+var heridasCtd;
+var camilla;
+var puntaje = 0;
+var heridaCurar;
+
+function inicio(){
+    inicializarVariables();
+    randomAnimales();
+    verAnimales(0);
+    verHeridas(0);
+    asignarEventosHerramientas();
 }
-setTimeout(desaparecer,3000);
 
 function inicializarVariables(){
-    intro = document.getElementById("pantalla_introduccion");
-    creditos = document.getElementById("creditos");
-    tortuga = document.getElementById("pantalla_tortuga");
-    foca = document.getElementById("pantalla_foca");
-    ballena = document.getElementById("pantalla_ballena");
-    histTortuga = document.getElementById("historia_tortuga");
-    histFoca = document.getElementById("historia_foca");
-    histBallena = document.getElementById("historia_ballena");
-}
+    camilla = document.getElementById("camilla");
+    heridasCtd = document.getElementById("heridasCtd");
+    heridas.push(new herida("p", "img/imgHeridas/basura2Ballena.png",5, "ballena"));
+    heridas.push(new herida("p", "img/imgHeridas/basuraBallena.png", 6, "ballena"));
+    heridas.push(new herida("p", "img/imgHeridas/basuraFoca.png", 4, "foca"));
+    heridas.push(new herida("b", "img/imgHeridas/basuraTortuga.png", 6, "tortuga"));
+    heridas.push(new herida("a", "img/imgHeridas/Cortada.png", 3, "todos"));
+    heridas.push(new herida("c", "img/imgHeridas/herida1.png", 4, "todos"));
+    heridas.push(new herida("e", "img/imgHeridas/Herida2.png", 3, "todos"));
+    heridas.push(new herida("a", "img/imgHeridas/herida4.png", 3, "todos"));
+    heridas.push(new herida("p", "img/imgHeridas/PaloTortuga.png", 3, "tortuga"));
+    heridas.push(new herida("c", "img/imgHeridas/Quemadura2.png", 3, "todos"));
+    heridas.push(new herida("p", "img/imgHeridas/VidrioTortuga.png", 7, "tortuga"));
+    randomAnima.push(new animalHerido("tortuga","img/Tortuga.png","heridasTortuga"));
+    randomAnima.push(new animalHerido("ballena","img/Ballena.png","heridasBallena"));
+    randomAnima.push(new animalHerido("foca","img/Foca.png","heridasFoca"));
+    boton("button1","b");
+    boton("button2","a");
+    boton("button3","c");
+    boton("button4","e");
+    boton("button5","p");
 
-function btnCreditos(){
-    inicializarVariables();
-    intro.style.display = "none";
-    creditos.style.display = "block";
-    tortuga.style.display = "none";
-    foca.style.display = "none";
-    ballena.style.display = "none";
-    histTortuga.style.display = "none";
-    histFoca.style.display = "none";
-    histBallena.style.display = "none";
-}
-
-function btnHome(){
-    inicializarVariables();
-    intro.style.display = "block";
-    creditos.style.display = "none";
-    tortuga.style.display = "none";
-    foca.style.display = "none";
-    ballena.style.display = "none";
-    histTortuga.style.display = "none";
-    histFoca.style.display = "none";
-    histBallena.style.display = "none";
-}
-
-function vector(){
-    var array = [];
-    inicializarVariables();
-    array[0]=tortuga.style.display;
-    array[1]=foca.style.display;
-    array[2]=ballena.style.display;
-
-    return array;
 }
 
 
-function btnPlay(){
-    var random;
-    var vector=[];
-    inicializarVariables();
-    intro.style.display = "none";
-    creditos.style.display = "none";
-    tortuga.style.display = "block";
-    foca.style.display = "none";
-    ballena.style.display = "none";
-    histTortuga.style.display = "none";
-    histFoca.style.display = "none";
-    histBallena.style.display = "none";
-    carga();
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+function randomAnimales(){
+    randomAnima = shuffle(randomAnima);
 }
 
-function btnPausa(){
-    inicializarVariables();
-    lightbox.className="mostrar";
-    btnCerrar.addEventListener("click",()=>{
-        lightbox.className="ocultar";
-    });
+function verAnimales(animal){
+   //camilla.className = "camilla "+randomAnima[animal];
+   camilla.style.background = "url('"+randomAnima[animal].url+"') no-repeat";
+   heridasCtd.className = randomAnima[animal].contenedorHeridas;
+   
 }
+function verHeridas(animal){
+    heridas = shuffle(heridas);
+    var temp;
+    for (const key in heridas) {
+        if(randomAnima[animal].nombre == heridas[key].animal || "todos" == heridas[key].animal){
+            heridasCtd.innerHTML = "<button onclick='tratarHerida(+heridas[key].herramienta+"')' class='btnsHeridas'><img src='"+heridas[key].urlImg+"'></button>"+heridasCtd.innerHTML;
+            //temp = document.getElementById(heridas[key].urlImg);
+            //temp.puntaje = heridas[key].puntos;
+            //temp.herramienta = heridas[key].herramienta;
+            //temp.addEventListener("click",tratarHerida);
+            //btnHeridas.push(temp);
+        }else{
 
-function carga(){
-    inicializarVariables();
-    if (tortuga.style.display=="block") {
-        cont_s = 0;
-        s=document.getElementById("segundos");
-
-        window.setInterval(function(){
-            s.innerHTML = cont_s;
-            cont_s++;
-            if (cont_s==30) {
-                
-            }
-        },1000);
+        }
     }
-    if (foca.style.display=="block") {
-        cont_s = 0;
-        s=document.getElementById("segundos2");
+   // asignarEventosHeridas();
+}
 
-        window.setInterval(function(){
-            s.innerHTML = cont_s;
-            cont_s++;
-            if (cont_s==30) {
-                
-            }
-        },1000);
+function asignarEventosHerramientas(){
+    for (const key in botones) {
+        botones[key].addEventListener("click",elegirHerramienta);
     }
-    if (ballena.style.display=="block") {
-        cont_s = 0;
-        s=document.getElementById("segundos3");
+}
 
-        window.setInterval(function(){
-            s.innerHTML = cont_s;
-            cont_s++;
-            if (cont_s==30) {
-                
-            }
-        },1000);
-    }   
+function tratarHerida(herramienta){
+    if(heridaCurar == herramienta){
+        puntaje+= 1;
+        //console.log(puntos);
+    }else{
+         puntaje-= 1;
+         //console.log(puntos);
+    }
+    heridaCurar = "";
+}
+function elegirHerramienta(){
+    heridaCurar = event.target.herramienta;
+    //console.log();
+}
+
+function animalHerido(nombre,url,contenedorHeridas){
+    this.nombre = nombre;
+    this.url = url;
+    this.contenedorHeridas = contenedorHeridas;
+}
+function herida(herramienta, urlImg, puntos,animal){
+    this.herramienta = herramienta;
+    this.urlImg = urlImg;
+    this.puntos = puntos;
+    this.animal = animal;
+    //this.boton = 
+}
+function boton(idBtn,herramienta){
+    var temp;
+    temp = document.getElementById(idBtn);
+    temp.herramienta = herramienta;
+    botones.push(temp);
 }
